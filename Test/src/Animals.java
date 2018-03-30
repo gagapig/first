1d0eb64ca93b2d90488b2c53c848aea46164a5c5
gagapig <gagapig@users.noreply.github.com>
2018-03-30 15:56:55 +0800

    Test

---
+++ Test/src/Animals.java
@@ -0,0 +1,13 @@
+/**
+ * Created by li.jiahong on 2018/3/23.
+ */
+public class Animals {
+    public static void main(String[] args) {
+
+        String abc = new String("abc");
+
+        System.out.println("Hello World!"+"Ele粉");
+    }
+
+
+}
